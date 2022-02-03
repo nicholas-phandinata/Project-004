3cba0a60728479f6844255b862328b0248a0631c
nicholas-phandinata <nicholas99.p@gmail.com>
2022-02-03 21:23:26 +0700

    Upload project 004 to github

---
+++ app/src/main/java/id/nicholasp/project004/MainActivity.java
@@ -0,0 +1,135 @@
+package id.nicholasp.project004;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
+
+import id.nicholasp.project004.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle toggle;
+    public Button btn_exit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initView();
+
+        btn_exit = findViewById(R.id.btn_exit);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_item, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    Fragment fragment = null;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_saldo:
+                fragment = new SaldoFragment();
+                getSupportActionBar().setTitle("Saldo");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+            case R.id.nav_obligasi:
+                fragment = new ObligasiFragment();
+                getSupportActionBar().setTitle("Obligasi");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+            case R.id.nav_crypto:
+                fragment = new CryptoFragment();
+                getSupportActionBar().setTitle("Crypto");
+                binding.drawer.closeDrawer(GravityCompat.START);
+                callFragment(fragment);
+                break;
+        }
+        return true;
+    }
+
+    private void initView() {
+        //custom toolbar
+        setSupportActionBar(binding.toolbar);
+
+        //default fragment dibuka pertama kali
+        getSupportActionBar().setTitle("Saldo");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, new SaldoFragment())
+                .commit();
+        binding.navView.setCheckedItem(R.id.nav_saldo);
+
+        //membuka drawer
+        toggle = new ActionBarDrawerToggle(this, binding.drawer, binding.toolbar,
+                R.string.open, R.string.close);
+
+        //drawer back button
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.white));
+
+        //sinkronisasi drawer
+        toggle.syncState();
+
+        //salah satu menu navigasi dipilih
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_saldo:
+                        fragment = new SaldoFragment();
+                        getSupportActionBar().setTitle("Saldo");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                    case R.id.nav_obligasi:
+                        fragment = new ObligasiFragment();
+                        getSupportActionBar().setTitle("Obligasi");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                    case R.id.nav_crypto:
+                        fragment = new CryptoFragment();
+                        getSupportActionBar().setTitle("Crypto");
+                        binding.drawer.closeDrawer(GravityCompat.START);
+                        callFragment(fragment);
+                        break;
+                }
+                return  true;
+            }
+        });
+    }
+
+    private void callFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.setCustomAnimations(
+                android.R.anim.slide_in_left, android.R.anim.slide_out_right
+        );
+        transaction.replace(R.id.frameLayout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+    }
+}
